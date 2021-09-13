bd63c37e61ee4d6a035310790c56e40f1ff3f1d5
bssgowtham <55312214+bssgowtham@users.noreply.github.com>
2021-09-13 14:30:33 +0530

    created the angular project and added the angular material package

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['post-create.component.css']
+})
+export class PostCreateComponent {
+  newPost = '';
+
+
+  onAddPost() {
+    this.newPost = `The user's post`;
+  }
+}
